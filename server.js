30b257f725c3c834dfc22724477b56ba44e5f10e
Kurtis Hight <gears2525@aol.com>
2022-02-06 20:14:58 -0800

    updated sever

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(express.static('public'));
 
 mongoose.connect(MONGODB_URI, {
 	useNewUrlParser: true,
-	useFindAndModify: false
+	useFindAndModify: false,
+	useUnifiedTopology: true,
 });
-
 
 // routes
 app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
-	console.log(`App running on port http://localhost:${PORT}/`);
+	console.log(`App running on port ${PORT}!`);
 });
